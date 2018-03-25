6fab761ce77e1d041c184e18356bd6a0fc811d65
Sinno <ts@sinno.dk>
2018-03-25 21:56:24 +0200

    Got it working! :)

--- examples/SOAP/dotnet/econSOAPExample/econSOAPExample/Program.cs
+++ examples/SOAP/dotnet/econSOAPExample/econSOAPExample/Program.cs
@@ -54,11 +54,33 @@ namespace econSOAPExample
                 var companyHandle = session.Company_Get();
                 var companyData = session.Company_GetData(companyHandle);
 
+                Console.WriteLine(companyData.Name);
+
                 var projectHandle = session.Project_FindByNumber(1844);
                 var timeEntriesData = session.Project_GetTimeEntries(projectHandle);
 
-                Console.WriteLine(companyData.Name);
-                Console.WriteLine();
+
+                //Fetch employees
+                var employeeHandles = session.Employee_GetAll();
+                foreach (EmployeeHandle employee in employeeHandles)
+                {
+                    var employeeData = session.Employee_GetData(employee);
+                    if (employeeData.EmployeeType == EmployeeType.TimeLogger)
+                    {
+                        Console.WriteLine(employeeData.Name + " CostPrice=" + employeeData.CostPrice + " SalesPrice=" + employeeData.SalesPrice);
+                        var timeEntries = session.Employee_GetTimeEntries(employee);
+                        foreach (TimeEntryHandle timeEntry in timeEntries)
+                        {
+                            var timeEntryData = session.TimeEntry_GetData(timeEntry);
+                            Console.WriteLine(timeEntryData.NumberOfHours + " " +timeEntryData.SalesPrice + " " + timeEntryData.Text);
+                        }
+                    }
+                  
+
+                    
+                }
+
+                
 
 
                 Console.WriteLine("Disconnecting");
