5f020056f425d135bec96a90d10f67f5a7554b51
Sinno <ts@sinno.dk>
2018-04-02 17:15:01 +0200

    Send email and save info to list

--- examples/SOAP/dotnet/econSOAPExample/econSOAPExample/Program.cs
+++ examples/SOAP/dotnet/econSOAPExample/econSOAPExample/Program.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.Mail;
+//using System.Net.Mail;
 using System.Net.Mime;
 using System.Reflection;
 using System.ServiceModel;
@@ -13,14 +13,34 @@ using System.Threading.Tasks;
 //Using the imported service reference. You may have another name for the reference.
 using econSOAPExample.econSoap;
 
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
+
 namespace econSOAPExample
 {
     class Program
     {
         static void Main(string[] args)
         {
-            PrintCompanyName();
+            GetEmployeeData();
+            EmailReportTLS();
         }
+
+        private struct EmployeeDataRecord
+        {
+            public string name;
+            public string email;
+            public decimal hours;
+            public decimal cost;
+            public decimal sales;
+            public decimal earned;
+            public decimal invoicedKPI;
+            public decimal productiveKPI;
+        }
+
+        private static List<EmployeeDataRecord> EmployeeDataRecords = new List<EmployeeDataRecord>();
 
         private static void Connect(EconomicWebServiceSoapClient session)
         {
@@ -44,8 +64,7 @@ namespace econSOAPExample
             }
         }
 
-
-        public static void PrintCompanyName()
+        public static void GetEmployeeData()
         {
             using (var session = new EconomicWebServiceSoapClient())
             {
@@ -56,8 +75,6 @@ namespace econSOAPExample
                 var companyData = session.Company_GetData(companyHandle);
 
                 Console.WriteLine(companyData.Name);
-
-
 
                 //Fetch and loop employees
                 var employeeHandles = session.Employee_GetAll();
@@ -83,7 +100,7 @@ namespace econSOAPExample
                         //var timeActivityData = session.Activity_GetData(timeActivityHandle);
                         //Console.WriteLine("Activity=" + timeActivityData.Name + " number=" + timeActivityData.Number);
 
-                        
+
                         //Filter on date interval
                         var res1 = timeEntryData.Date.CompareTo(fromDate);
                         var res2 = timeEntryData.Date.CompareTo(toDate);
@@ -97,30 +114,76 @@ namespace econSOAPExample
 
                         }
 
+                        //Output individual timeentry data
                         //Console.WriteLine(timeEntryData.Date + ":" + timeEntryData.NumberOfHours + " " + timeEntryData.SalesPrice + " " + timeEntryData.Text);
                     }
 
-
                     Console.WriteLine(employeeData.Name + " \t TimeregHours=" + employeeHourSum + " \t Cost=" + employeeCostSum + " \t Sales=" + employeeSalesSum + " \t Earned=" + employeeEarnedSum);
 
-
-                    if (employeeData.EmployeeType == EmployeeType.TimeLogger)
-                    {
-
-                    }
-
-
-
+                    //Add record to the list
+                    EmployeeDataRecord temp;
+                    temp.name = employeeData.Name;
+                    temp.email = "ts@sinno.dk";
+                    temp.hours = employeeHourSum;
+                    temp.cost = employeeCostSum;
+                    temp.sales = employeeSalesSum;
+                    temp.earned = employeeEarnedSum;
+                    temp.invoicedKPI = 0;
+                    temp.productiveKPI = 0;
+                    EmployeeDataRecords.Add(temp);
                 }
-
-
-
 
                 Console.WriteLine("Disconnecting");
                 session.Disconnect();
-                Console.WriteLine("Done");
+                Console.WriteLine("Done - Press any key to proceed");
                 Console.ReadKey();
+            }
+        }
 
+        private static string PrepareEmailMessage()
+        {
+            string returnString;
+
+            returnString = @"Hej,
+
+Dette er din månedlige statusmail. Din udfaktureringsgrad har været 93% og din produktivitetsgrad har været på 100%.
+
+Det lever fint op til forventningerne.
+
+Mvh Thomas";
+
+            return returnString;
+        }
+
+        public static void EmailReportTLS()
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Sinno Office", "office@sinno.dk"));
+            message.To.Add(new MailboxAddress("Thomas Schmidt", "ts@sinno.dk"));
+            message.Subject = "Månedsrapport";
+            message.Body = new TextPart("plain") { Text = PrepareEmailMessage() };
+
+            using (var client = new SmtpClient())
+            {
+                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                // Connect using TLS (Office 365 requirement)
+                Console.WriteLine("Connect to SMTP Server using TLS");
+                client.Connect("smtp.office365.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+
+                // Authenticate using login credentials
+                Console.WriteLine("Authenticate Login");
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate("ts@sinno.dk", "");
+
+                //Send email and disconnect from smtp-server
+                Console.WriteLine("Send message");
+                client.Send(message);
+                client.Disconnect(true);
+
+                Console.WriteLine("Done - Press any key to proceed");
+                Console.ReadKey();
             }
         }
     }
